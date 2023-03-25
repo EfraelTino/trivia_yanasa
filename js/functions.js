5d5e04f30e08ff64abc71560aec0c835925b6aa9
efrael <efrael2001@gmail.com>
2023-03-25 23:23:56 +0100

    first commit

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+$(".btn-oc-modal-terminos").click(function () {
+  $(".div-nav-terminos").hasClass("active")
+    ? $(".div-nav-terminos").removeClass("active")
+    : $(".div-nav-terminos").addClass("active");
+});
